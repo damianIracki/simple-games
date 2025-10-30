f17e23c6d4097ae94b60d9f0f34e168723beca42
damian.iracki13@gmail.com <?zVeW4Vo@h8V330!+{0y0Krs>
2025-10-30 18:21:35 +0100

    Initial commit

---
+++ src/test/java/com/example/simple_games/SimpleGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
